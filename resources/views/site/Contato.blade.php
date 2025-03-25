0e8a98d5d85915f59bf54222ee4f63888d1d8f8d
otaviolt <otavio.torraca@etec.sp.gov.br>
2025-03-24 21:34:13 -0300

    projeto

---
+++ resources/views/site/Contato.blade.php
@@ -0,0 +1,7 @@
+<h3>Contato</h3>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre-nós</a></li>
+</ul>
---
+++ resources/views/site/SobreNos.blade.php
@@ -0,0 +1,7 @@
+<h3>Sobre-nós</h3>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre-nós</a></li>
+</ul>
